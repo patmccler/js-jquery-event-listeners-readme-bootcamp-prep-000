09ff06ffbc0612269786ca0bb36ee9df5ca70e1b
Patrick McClernan <patmccler@gmail.com>
2017-10-27 23:59:02 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -9,6 +9,15 @@ $(document).ready(function(){
 function getIt() {
   $('p').on('click', function(){
     alert("Hey!")
+  })
+}
 
+function frameIt() {
+  var img = $('img')[0]
+  img.on('load', function(){
+      img.className += "tasty"
+      img.style.borderColor = "red"
+
+    
   })
 }
