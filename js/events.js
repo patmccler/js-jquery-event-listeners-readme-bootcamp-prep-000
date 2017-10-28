2bd8d9955f5189984aa3bc8d91182d2165e1ae34
Patrick McClernan <patmccler@gmail.com>
2017-10-28 00:19:41 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -5,3 +5,35 @@ $(document).ready(function(){
 // call functions here
 
 });
+
+function getIt() {
+  $('p').on('click', function(){
+    alert("Hey!")
+  })
+}
+
+function frameIt() {
+
+  $('img').on('load', function(){
+      img = $('img')[0]
+      img.className += "tasty"
+      img.style.borderColor = "red"
+  })
+}
+
+function pressIt() {
+  $('form').on('keydown', function(key) {
+    if( key.which === 71)
+      alert("YOU PRESSED G")
+
+  })
+}
+
+function submitIt() {
+  $("form").on("submit", function() {
+
+    alert("Your form is going to be submitted now.")
+
+  })
+
+}
